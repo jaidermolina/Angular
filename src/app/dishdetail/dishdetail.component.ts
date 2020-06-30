5c8ca58324a663772330051a3abe59c94ddd59cb
Jaider Molina <jaider.molina@udea.edu.co>
2020-06-29 23:40:01 -0500

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,17 +6,30 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import {switchMap} from 'rxjs/operators';
 import {Comment} from '../shared/comment';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
   @ViewChild('fform') commentFormDirective;
-
+  visibility = 'shown';
   dish: Dish;
   dishIds: string[];
   prev: string;
@@ -51,10 +64,9 @@ export class DishdetailComponent implements OnInit {
     const id = this.route.snapshot.params.id;
     this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params
-    .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.dishcopy=dish; this.setPrevNext(dish.id); }, 
-    errmess => this.errMess = <any>errmess);
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+    .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+      errmess => this.errMess = <any>errmess);
 
     this.createForm();
   }
